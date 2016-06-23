e5feac0c811963cb52be211c745dade54dd70ef1
584306 <robertkelly@C02P71QBG3QP.local>
2016-06-23 11:00:06 -0400

    refactored scrapers

--- scrapers/pictaram_scraper.py
+++ scrapers/pictaram_scraper.py
@@ -1,26 +1,8 @@
 import time
 import ssl
 
-
-from selenium import webdriver
 from bs4 import BeautifulSoup
 from urllib2 import urlopen
-
-
-def pikore_tag_scraper(tag, num_scrolls):
-
-    BASE_URL = "http://www.pikore.com/tag/" + tag
-    driver = webdriver.Chrome()  # Optional argument, if not specified will search path.
-    driver.get(BASE_URL)
-    time.sleep(1) # Let the user actually see something!
-
-    for i in range(1, num_scrolls):
-           driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-           time.sleep(1)
-
-    html_source = driver.page_source
-
-    return html_source
 
 
 def scrape_pictaram(page_url, pages=1, print_progress=False):
---
+++ scrapers/pikore_scraper.py
@@ -0,0 +1,19 @@
+import time
+
+from selenium import webdriver
+
+
+def pikore_tag_scraper(tag, num_scrolls):
+
+    BASE_URL = "http://www.pikore.com/tag/" + tag
+    driver = webdriver.Chrome()  # Optional argument, if not specified will search path.
+    driver.get(BASE_URL)
+    time.sleep(1) # Let the user actually see something!
+
+    for i in range(1, num_scrolls):
+           driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+           time.sleep(1)
+
+    html_source = driver.page_source
+
+    return html_source
